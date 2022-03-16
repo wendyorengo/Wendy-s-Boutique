984d3216752f9dfa332b0a33ce93ab31639052c6
mercy-yugi <mercyyugi@gmail.com>
2022-03-16 14:54:51 +0300

    resolve conflict

---
+++ project1.py
@@ -0,0 +1,39 @@
+print("good morning mercy")
+thistuple= ("mercy","yugi","achieng")
+print(thistuple)
+print(len(thistuple))
+print(type(thistuple))
+names=tuple(("morning","how are you doing"))
+print(names)
+
+#x=("apple", "banana","cherryy")
+#x[1]="mango"
+
+#print(x)
+
+thisdict={
+    "model":"toyota",
+    "type":"mark x",
+    "year":2022
+}
+print(thisdict)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
